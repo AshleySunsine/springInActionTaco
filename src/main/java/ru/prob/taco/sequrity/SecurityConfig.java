fc9e6077e93f8617be910eb4202f404ae3de51df
asp-psm <sgt16@yandex.ru>
2022-10-12 11:07:21 +0000

    Сохранение ингредиентов через REST public SecurityFilterChain filterChain тоже пришлось подправить. Не как в книжке. Но теперь работает.

--- src/main/java/ru/prob/taco/sequrity/SecurityConfig.java
+++ src/main/java/ru/prob/taco/sequrity/SecurityConfig.java
@@ -37,13 +37,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                .authorizeRequests()
-               .antMatchers(HttpMethod.POST, "/api/ingredients")
+                .antMatchers("/data-api/tacos", "/data-api/orders/**")
+                .permitAll()
+                .antMatchers("/**").access("permitAll")
+               .antMatchers(HttpMethod.POST, "/data-api/ingredients")
                .hasAuthority("SCOPE_writeIngredients")
-               .antMatchers(HttpMethod.DELETE, "/api//ingredients")
+               .antMatchers(HttpMethod.DELETE, "/data-api//ingredients")
                .hasAuthority("SCOPE_deleteIngredients")
-               .antMatchers("/api//tacos", "/api//orders/**")
-               .permitAll()
-               .antMatchers("/**").access("permitAll")
+
                .and()
                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
                .httpBasic()
@@ -59,8 +60,11 @@ public class SecurityConfig {
 /*
 curl localhost:8080/ingredients -H"Content-type: application/json" -d'{"id":"CRKT", "name":"Legless Crickets", "type":"PROTEIN"}'
 
-curl localhost:8080/api/ingredients -H"Content-type: application/json" -d'{"id":"SHMP", "name":"Coconut Shrimp", "type":"PROTEIN"}' -H"Authorization: Bearer eyJraWQiOiJiYjBiNjBlZC05YzE2LTQ5MjItOWNiNi00NjJkYWVlZTYwYzQiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJxIiwiYXVkIjoidGFjby1hZG1pbi1jbGllbnQiLCJuYmYiOjE2NjU1NzAwMTcsInNjb3BlIjpbImRlbGV0ZUluZ3JlZGllbnRzIiwid3JpdGVJbmdyZWRpZW50cyJdLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6OTAwMCIsImV4cCI6MTY2NTU3MDMxNywiaWF0IjoxNjY1NTcwMDE3fQ.gkScKwiVYRNw-Zq0acRVpNbf7g5HRk39B2XQa-MZ75Hkg4R508wlTq_Ae7ciZs_cgZORXpNSN7SjVc7-DUOAJDn4HTLUxuXfm_0vg3VPnmylrUm310uvatBmUZodHyg8_8GD7EY-uW6HqaDU-vEFfNQFPZHtnwCiQCgQpqbjzE0QRAn-iuc62Jmiop9CRGFTgfgO4UdkCaE-Gbc9oDCIOoR2-5h2oCD-5CAk5RkH9GdJz2Yftgx7e29d25AOheqpMsR-APF4RpFbMucNv6FsihZdBoQ2zzqsyiGp7282GmkvuI2m91d6yE5Zi_JqtatEnZsG3ESyCU1grswZJcHdrg"
+add:
+curl localhost:8080/data-api/ingredients -H"Content-type: application/json" -d'{"id":"SHMP", "name":"Coconut Shrimp", "type":"PROTEIN"}' -H"Authorization: Bearer eyJraWQiOiJiYjBiNjBlZC05YzE2LTQ5MjItOWNiNi00NjJkYWVlZTYwYzQiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJxIiwiYXVkIjoidGFjby1hZG1pbi1jbGllbnQiLCJuYmYiOjE2NjU1NzE2OTgsInNjb3BlIjpbImRlbGV0ZUluZ3JlZGllbnRzIiwid3JpdGVJbmdyZWRpZW50cyJdLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6OTAwMCIsImV4cCI6MTY2NTU3MTk5OCwiaWF0IjoxNjY1NTcxNjk4fQ.I2otXTpYzvgi3O5FSBb3TJCAko5LSVRaj9f664nXyITc7BPMyBu4AFuL3G1oywlavWUUrKoGnbESNCVqVtE9HmSD9-QSex2SOzOfIob83akG4bybC59FirTisE6_0gEVFdUIVoLQkdir9_-QLyHEx2b1CW2q_8VfIRCZzqJjLSKbEpYlkNwsYRxaunOEWQwYG9OdeXTNnWBhl9G-QNctwEwsEObA_O-ssw1F7bMDgJTQ5-Ai_sGRil68AJ3upAaBgSdy4DCijvqut9wiMBvyCzvgt0FcP5uaA14WrETD-BHtfnG2zxeRP-L15DhzogEHmDqrDjqXvKyMqQuoPoG-Kw"
 
+refrash:
+curl localhost:9000/oauth2/token -H"Content-type: application/x-www-form-urlencoded" -d"grant_type=refresh_token&refresh_token=$refreshToken" -u taco-admin-client:secret
 */
 
 }
